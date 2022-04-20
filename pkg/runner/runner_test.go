a38adf4de7e73de0875274f5bb9ecd5ddc7f6178
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-04-20 21:58:44 +0200

    Fixed tests.

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -1,7 +1,9 @@
 package runner
 
 import (
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
@@ -13,11 +15,13 @@ func TestRun(t *testing.T) {
 	tempDir := os.TempDir()
 	os.Setenv("RUNNER_DATADIR", tempDir)
 
-	t.Run("runner should run test based on execution data", func(t *testing.T) {
+	t.Run("basic Karate test feature", func(t *testing.T) {
 		// given
 		runner := NewRunner()
 		execution := testkube.NewQueuedExecution()
-		execution.Content = testkube.NewStringTestContent("hello I'm test content")
+		execution.Content = testkube.NewStringTestContent("")
+		execution.TestType = "karate/feature"
+		writeTestContent(t, tempDir, "../../examples/karate-test.feature")
 
 		// when
 		result, err := runner.Run(*execution)
@@ -26,5 +30,16 @@ func TestRun(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, result.Status, testkube.ExecutionStatusPassed)
 	})
+}
 
+func writeTestContent(t *testing.T, dir string, file string) {
+	featureFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		assert.FailNow(t, "Unable to read Karate feature file")
+	}
+
+	err = ioutil.WriteFile(filepath.Join(dir, "test-content"), featureFile, 0644)
+	if err != nil {
+		assert.FailNow(t, "Unable to write Karate feature file as test content")
+	}
 }
