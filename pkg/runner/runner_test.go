0fd0be0e6e514b4540d1ac9631038853debfbd99
ThumNet <jeffreytummers@gmail.com>
2023-03-31 14:28:13 +0200

    Add support to start karate in Repository directory with specified Path

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -1,7 +1,6 @@
 package runner
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
@@ -48,16 +47,93 @@ func TestRun(t *testing.T) {
 		assert.Equal(t, result.Status, testkube.ExecutionStatusFailed)
 		assert.Len(t, result.Steps, 1)
 	})
+
+	t.Run("project Karate test without repo path", func(t *testing.T) {
+		// given
+		runner := NewRunner()
+		execution := testkube.NewQueuedExecution()
+		repository := testkube.NewGitRepository("http://not-used/", "main")
+		execution.Content = &testkube.TestContent{Type_: "git-dir", Repository: repository}
+		execution.TestType = "karate/project"
+		execution.Args = []string{"."} // tell karate to look for features in start dir
+		writeTestContentProject(t, execution.Content.Repository, tempDir, "../../examples/project")
+
+		// when
+		result, err := runner.Run(*execution)
+
+		// then
+		assert.NoError(t, err)
+		assert.Equal(t, result.Status, testkube.ExecutionStatusPassed)
+		assert.Len(t, result.Steps, 2)
+	})
+
+	t.Run("project Karate test with repo path", func(t *testing.T) {
+		// given
+		runner := NewRunner()
+		execution := testkube.NewQueuedExecution()
+		repository := testkube.NewGitRepository("http://not-used/", "main").WithPath("my-dir")
+		execution.Content = &testkube.TestContent{Type_: "git-dir", Repository: repository}
+		execution.TestType = "karate/project"
+		execution.Args = []string{"."} // tell karate to look for features in start dir
+		writeTestContentProject(t, execution.Content.Repository, tempDir, "../../examples/project")
+
+		// when
+		result, err := runner.Run(*execution)
+		// then
+		assert.NoError(t, err)
+		assert.Equal(t, result.Status, testkube.ExecutionStatusPassed)
+		assert.Len(t, result.Steps, 2)
+	})
 }
 
 func writeTestContent(t *testing.T, dir string, file string) {
-	featureFile, err := ioutil.ReadFile(file)
+	featureFile, err := os.ReadFile(file)
 	if err != nil {
 		assert.FailNow(t, "Unable to read Karate feature file")
 	}
 
-	err = ioutil.WriteFile(filepath.Join(dir, "test-content"), featureFile, 0644)
+	err = os.WriteFile(filepath.Join(dir, "test-content"), featureFile, 0644)
 	if err != nil {
 		assert.FailNow(t, "Unable to write Karate feature file as test content")
+	}
+}
+
+func writeTestContentProject(t *testing.T, repo *testkube.Repository, targetDir string, sourceDir string) {
+
+	repoDir := filepath.Join(targetDir, "repo")
+	if len(repo.Path) > 0 {
+		repoDir = filepath.Join(repoDir, repo.Path)
+	}
+
+	err := os.RemoveAll(repoDir)
+	if err != nil {
+		assert.FailNow(t, "Unable to clear repoDir")
+	}
+
+	_, err = os.Stat(repoDir)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(repoDir, 0777)
+		if err != nil {
+			assert.FailNow(t, "Unable to create repoDir")
+		}
+	}
+
+	files, err := os.ReadDir(sourceDir)
+	if err != nil {
+		assert.FailNow(t, "Unable to read sourceDir")
+	}
+	for _, f := range files {
+		if !f.IsDir() {
+			file := filepath.Join(sourceDir, f.Name())
+			featureFile, err := os.ReadFile(file)
+			if err != nil {
+				assert.FailNow(t, "Unable to read Karate feature file")
+			}
+
+			err = os.WriteFile(filepath.Join(repoDir, f.Name()), featureFile, 0644)
+			if err != nil {
+				assert.FailNow(t, "Unable to write Karate feature file as test content")
+			}
+		}
 	}
 }
